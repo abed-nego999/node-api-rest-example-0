09e63335b712da9e8b83abb6a51065ac9cc60353
Esteban Saugar <e_saugar_servext_rsi@cajarural.com>
2019-04-03 11:41:42 +0200

    Hola

--- index.js
+++ index.js
@@ -20,4 +20,24 @@ mongoose.connect("mongodb+srv://cluster0-f3avm.mongodb.net/test", (err, res) => 
     console.log(`ERROR: connecting to Database. ${ err }`);
   }
   app.listen(PORT, () => console.log(`Node server running on http://localhost:${ PORT }`));
+
+  var kittySchema = new mongoose.Schema({
+    name: String
+  });
+  // NOTE: methods must be added to the schema before compiling it with mongoose.model()
+  kittySchema.methods.speak = function () {
+    var greeting = this.name
+      ? "Meow name is " + this.name
+      : "I don't have a name";
+    console.log(greeting);
+  }
+  var Kitten = mongoose.model('Kitten', kittySchema);
+  var silence = new Kitten({ name: 'Silence' });
+  console.log(silence.name); // 'Silence'
+  var fluffy = new Kitten({ name: 'fluffy' });
+  fluffy.speak(); // "Meow name is fluffy"
+  fluffy.save(function (err, fluffy) {
+    if (err) return console.error(err);
+    fluffy.speak();
+  });
 });
