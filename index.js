cebfa81d795edde3e42af422dacdb4d657af823e
Esteban Saugar <e_saugar_servext_rsi@cajarural.com>
2019-04-03 11:55:14 +0200

    Pues eso

--- index.js
+++ index.js
@@ -15,29 +15,45 @@ var router = express.Router();
 router.get('/', (req, res) => res.send("Hello World!"));
 
 app.use(router);
-mongoose.connect("mongodb+srv://cluster0-f3avm.mongodb.net/test?authSource=admin", {user: "admin", pass: "vsusm,mqlc999"}, (err, res) => {
-  if (err) {
-    console.log(`ERROR: connecting to Database. ${ err }`);
-  }
-  app.listen(PORT, () => console.log(`Node server running on http://localhost:${ PORT }`));
+mongoose.connect("mongodb+srv://cluster0-f3avm.mongodb.net/test?authSource=admin",
+  {user: "admin", pass: "vsusm,mqlc999"},
+  (err, res) => {
+    if (err) {
+      console.log(`ERROR: connecting to Database. ${ err }`);
+    }
+    app.listen(PORT, () => console.log(`Node server running on http://localhost:${ PORT }`));
 
-  var kittySchema = new mongoose.Schema({
-    name: String
+    var kittySchema = new mongoose.Schema({
+      name: String
+    });
+    // NOTE: methods must be added to the schema before compiling it with mongoose.model()
+    kittySchema.methods.speak = () => {
+      var greeting = this.name
+        ? "Meow name is " + this.name
+        : "I don't have a name";
+      console.log(greeting);
+    }
+    var Kitten = mongoose.model('Kitten', kittySchema);
+    var silence = new Kitten({ name: 'Silence' });
+    console.log(silence.name); // 'Silence'
+    var fluffy = new Kitten({ name: 'fluffy' });
+    fluffy.speak(); // "Meow name is fluffy"
+
+    // Guarda a fluffy en MongoDB
+    fluffy.save((err, cat) => {
+      if (err) return console.error(`Error al guardar a ${ cat.name }: ${ err }`);
+      cat.speak();
+    });
+
+    // Guarda a silence en MongoDB
+    silence.save((err, cat) => {
+      if (err) return console.error(`Error al guardar a ${ cat.name }: ${ err }`);
+      cat.speak();
+    });
+
+    // Busca a fluffy en MongoDB
+    Kitten.find((err, kittens) => {
+      if (err) return console.error(err);
+      console.log(kittens);
+    });
   });
-  // NOTE: methods must be added to the schema before compiling it with mongoose.model()
-  kittySchema.methods.speak = function () {
-    var greeting = this.name
-      ? "Meow name is " + this.name
-      : "I don't have a name";
-    console.log(greeting);
-  }
-  var Kitten = mongoose.model('Kitten', kittySchema);
-  var silence = new Kitten({ name: 'Silence' });
-  console.log(silence.name); // 'Silence'
-  var fluffy = new Kitten({ name: 'fluffy' });
-  fluffy.speak(); // "Meow name is fluffy"
-  fluffy.save(function (err, fluffy) {
-    if (err) return console.error(err);
-    fluffy.speak();
-  });
-});
